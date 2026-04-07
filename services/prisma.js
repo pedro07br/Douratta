7063bf43bca6c8a8344b03882b366de3ecfa0646
Pedro Henrique <Pedro07br@gmail.com>
2026-04-07 17:50:36 -0300

    fix: prisma.js caminho correto para generated/prisma

--- services/prisma.js
+++ services/prisma.js
@@ -1,10 +1,12 @@
-import { PrismaClient } from '../generated/prisma/index.js'
+import { PrismaClient } from '../generated/prisma'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL)
+const globalForPrisma = globalThis
 
-const prisma = new PrismaClient({ adapter })
+const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  adapter: new PrismaMariaDb(process.env.DATABASE_URL)
+})
 
-await prisma.$connect()
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
